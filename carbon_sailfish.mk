f2b161a775fae6289290d4ca1aa0828804cd5407
shagbag913 <sh4gbag913@gmail.com>
2018-08-14 17:42:40 -0400

    sailfish: Update to PPR1.180610.009
    - first Pie fingerprint
    - include device-carbon.mk
    
    Change-Id: I321001c7fe93136f647d79c03888e9e710eb18fb
    Signed-off-by: shagbag913 <sh4gbag913@gmail.com>

--- carbon_sailfish.mk
+++ carbon_sailfish.mk
@@ -11,6 +11,8 @@ $(call inherit-product, vendor/carbon/config/common.mk)
 # Inherit device configuration
 $(call inherit-product, device/google/marlin/aosp_sailfish.mk)
 
+-include device/google/marlin/device-carbon.mk
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := carbon_sailfish
 PRODUCT_BRAND := google
@@ -20,8 +22,8 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=sailfish \
-    BUILD_FINGERPRINT=google/sailfish/sailfish:8.1.0/OPM4.171019.021.P1/4820305:user/release-keys \
-    PRIVATE_BUILD_DESC="sailfish-user 8.1.0 OPM4.171019.021.P1 4820305 release-keys"
+    BUILD_FINGERPRINT=google/sailfish/sailfish:9/PPR1.180610.009/4898911:user/release-keys \
+    PRIVATE_BUILD_DESC="sailfish-user 9 PPR1.180610.009 4898911 release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.carbon.maintainer="shagbag913"
